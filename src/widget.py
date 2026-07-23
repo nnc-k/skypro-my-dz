8c9fb99a452f5d8043535cde55a1074aeab271fa
nancy k <katkova.a1997@gmail.com>
2026-07-23 17:16:49 +0300

    Добавлена функция get_date

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
@@ -12,3 +14,12 @@ def mask_account_card(data: str) -> str:
         return f"{name} {get_mask_account(number)}"
 
     return f"{name} {get_mask_card_number(number)}"
+
+
+def get_date(date_string: str) -> str:
+    """
+    Возвращает дату в формате ДД.ММ.ГГГГ.
+    """
+
+    date = datetime.fromisoformat(date_string)
+    return date.strftime("%d.%m.%Y")
