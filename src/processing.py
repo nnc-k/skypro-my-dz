949db269593fd9140e22262028164cdee2ac70d2
nancy k <katkova.a1997@gmail.com>
2026-07-31 11:09:12 +0300

    readme обновлен

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,4 @@
-def filter_by_state(data: list[dict], state: str = "EXECUTED"
-) -> list[dict]:
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Возвращает список словарей с указанным значением state.
     """
