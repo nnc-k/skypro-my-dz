8a06fb5bae72160130f79ccf88f9420486fb861b
nancy k <katkova.a1997@gmail.com>
2026-08-03 21:13:12 +0300

    редактирование файла test_masks.py

--- tests/tests_masks.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from src.masks import get_mask_account
-
-
-def test_get_mask_account():
-    assert get_mask_account("1234567890123456") == "**3456"
-    assert get_mask_account("1234560") == "**4560"
-    assert get_mask_account("012") == ValueError("Некорректный номер счета.")
