f67f051bdc7c455fb64f382a3e7d26eec7cd40f7
nancy k <katkova.a1997@gmail.com>
2026-08-03 20:45:38 +0300

    Создание новой ветки и файла tests_masks.py

---
+++ tests/tests_masks.py
@@ -0,0 +1,7 @@
+from src.masks import get_mask_account
+
+
+def test_get_mask_account():
+    assert get_mask_account("1234567890123456") == "**3456"
+    assert get_mask_account("1234560") == "**4560"
+    assert get_mask_account("012") == ValueError("Некорректный номер счета.")
