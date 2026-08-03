8e7c85ae59ebcb45408e5aefdaef837cb5cb3b7e
nancy k <katkova.a1997@gmail.com>
2026-08-03 21:19:51 +0300

    добавление файла test_widget.py

---
+++ tests/test_widget.py
@@ -0,0 +1,24 @@
+import pytest
+
+from src.widget import get_date, mask_account_card
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        (
+            "Visa Platinum 7000792289606361",
+            "Visa Platinum 7000 79** **** 6361",
+        ),
+        (
+            "Счет 73654108430135874305",
+            "Счет **4305",
+        ),
+    ],
+)
+def test_mask_account_card(value, expected):
+    assert mask_account_card(value) == expected
+
+
+def test_get_date():
+    assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
