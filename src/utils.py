2572d3e5c7df56ce4dbacc1fcf1939ce67611aaa
nancy k <katkova.a1997@gmail.com>
2026-08-28 17:15:52 +0300

    добавлено логирование в модуле utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,24 @@
 import json
+import logging
+import os
+
+os.makedirs("logs", exist_ok=True)
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler(
+    "logs/utils.log",
+    mode="w",
+    encoding="utf-8"
+)
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+file_handler.setFormatter(file_formatter)
+
+if not logger.handlers:
+    logger.addHandler(file_handler)
 
 
 def read_json_file(file_path: str) -> list[dict]:
@@ -8,9 +28,16 @@ def read_json_file(file_path: str) -> list[dict]:
             data = json.load(file)
 
         if isinstance(data, list):
+            logger.info("JSON-файл %s успешно прочитан", file_path)
             return data
 
+        logger.error("JSON-файл %s содержит данные не в виде списка", file_path)
         return []
 
-    except (FileNotFoundError, json.JSONDecodeError):
+    except FileNotFoundError:
+        logger.error("JSON-файл %s не найден", file_path)
+        return []
+
+    except json.JSONDecodeError:
+        logger.error("Ошибка декодирования JSON-файла %s", file_path)
         return []
